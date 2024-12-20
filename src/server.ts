d757d803e3ef68556842c776b671045820ff5b66
henrygoncalvess <octanebt@gmail.com>
2024-12-20 20:16:44 -0300

    novos pacotes e rota com informações OK

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 
 const app = fastify();
+
+app.register(fastifyCors, { origin: '*' });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifyCors, { origin: '*' });
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "API auto-documentável"
+        }
+    }
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+})
 
 app.listen({ port: 3000 }, () => {
     console.log('running...');
